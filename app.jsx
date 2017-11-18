0c75a5b2e4fc04b47c7b202ffe2fa5dd75001d74
Gloria Jimenez <gelenaj@gmail.com>
2017-11-18 13:08:43 -0500

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,11 @@
+function Application() {
+    return(
+        <div>
+            <h1>This is my first application using React</h1>
+            <p>I am learning the basics</p>
+        </div>
+    );
+
+}
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
